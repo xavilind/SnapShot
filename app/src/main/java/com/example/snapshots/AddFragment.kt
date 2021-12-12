6737f66297a9d6c66f617f874293a49aa9a712f3
avilind <xavilind7@gmail.com>
2021-12-12 17:18:12 -0600

    AddFragment

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -1,20 +1,45 @@
 package com.example.snapshots
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.snapshots.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+
+    private val RC_GELLERY = 18
+
+    private  lateinit var mBinding: FragmentAddBinding
+
+    private var mPhotoSelectUri: Uri? = null
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        mBinding = FragmentAddBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.btnPost.setOnClickListener { postSnapshot() }
+
+        mBinding.btnSelect.setOnClickListener { openGallery() }
+    }
+
+    private fun openGallery(){
+
+    }
+
+    private fun postSnapshot() {
+
     }
 
 }
