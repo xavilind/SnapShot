ef2dd4064c0c020b56cd504daafa109d2b96fafe
avilind <xavilind7@gmail.com>
2021-12-12 14:17:15 -0600

    Registro-FireBase-Adapter

--- app/src/main/java/com/example/snapshots/HomeFragment.kt
+++ app/src/main/java/com/example/snapshots/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.snapshots.databinding.ItemSnapshotBinding
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +16,14 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    inner class SnapshotHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot){
+
+        }
     }
 
 }
