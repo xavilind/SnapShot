ca71b79ebdf62698b7949a2cd619e0e2b40b85f1
avilind <xavilind7@gmail.com>
2021-12-12 17:24:43 -0600

    DiseñoVista 2

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -1,7 +1,10 @@
 package com.example.snapshots
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +13,8 @@ import com.example.snapshots.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
-    private val RC_GELLERY = 18
+    private val RC_GALLERY = 18
+    private
 
     private  lateinit var mBinding: FragmentAddBinding
 
@@ -35,11 +39,24 @@ class AddFragment : Fragment() {
     }
 
     private fun openGallery(){
-
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, RC_GALLERY)
     }
 
     private fun postSnapshot() {
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == RC_GALLERY){
+                mPhotoSelectUri = data?.data
+                mBinding.imgPhoto.setImageURI(mPhotoSelectUri)
+                mBinding.tilTitle.visibility = View.VISIBLE
+                mBinding.tvMessage.text = getString(R.string.post_message_valid_title)
+            }
+        }
     }
 
 }
