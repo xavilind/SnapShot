0c35f48534a9b241ac19a223dc999cc69927660f
avilind <xavilind7@gmail.com>
2021-12-12 17:40:18 -0600

    Subir imagen a Cloud Storage

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -10,13 +10,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.snapshots.databinding.FragmentAddBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class AddFragment : Fragment() {
 
     private val RC_GALLERY = 18
-    private
+    private val PATH_SNAPSHOT = "snapshots"
 
     private  lateinit var mBinding: FragmentAddBinding
+    private lateinit var mStoreageReference: StorageReference
+    private lateinit var mDatabaseReference: DatabaseReference
 
     private var mPhotoSelectUri: Uri? = null
 
@@ -36,6 +43,9 @@ class AddFragment : Fragment() {
         mBinding.btnPost.setOnClickListener { postSnapshot() }
 
         mBinding.btnSelect.setOnClickListener { openGallery() }
+
+        mStoreageReference = FirebaseStorage.getInstance().reference
+        mDatabaseReference = FirebaseDatabase.getInstance().reference.child(PATH_SNAPSHOT)
     }
 
     private fun openGallery(){
@@ -44,7 +54,32 @@ class AddFragment : Fragment() {
     }
 
     private fun postSnapshot() {
+        mBinding.progressBar.visibility = View.VISIBLE
+        //mStoreageReference.child(PATH_SNAPSHOT).child("my_photo")
+        val storageReference = mStoreageReference.child(PATH_SNAPSHOT).child("mu_photo")
+        if(mPhotoSelectUri != null){
+            storageReference.putFile(mPhotoSelectUri!!)
+                .addOnProgressListener {
+                    val progress = (100 * it.bytesTransferred/it.totalByteCount).toDouble()
+                    mBinding.progressBar.progress = progress.toInt()
+                    mBinding.tvMessage.text = "$progress"
+                }
+                .addOnCompleteListener{
+                    mBinding.progressBar.visibility = View.VISIBLE
+                }
+                .addOnSuccessListener {
+                    Snackbar.make(mBinding.root, "Instantánea publicada",
+                        Snackbar.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener{
+                    Snackbar.make(mBinding.root, "No se pudo subir, intente más tarde.",
+                        Snackbar.LENGTH_SHORT).show()
+                }
+        }
+    }
 
+    private fun saveSnapshot(){
+        
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -58,5 +93,6 @@ class AddFragment : Fragment() {
             }
         }
     }
+
 
 }
